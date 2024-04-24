e41b3609a101b71a4a89630fb273a131c70504eb
Vimal <vimalthapliyal126@gmail.com>
2024-04-24 14:29:24 +0530

    add user modal

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
-  console.log(name, email, password);
+
   // Validation
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  //  Database Calls
+
+  const user = await userModel.findOne({ email: email });
+
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email");
     return next(error);
   }
 
