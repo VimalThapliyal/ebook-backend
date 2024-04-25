c366ce857372f28690cbdeb6e12fc566ede26eb4
Vimal <vimalthapliyal126@gmail.com>
2024-04-25 11:24:51 +0530

    Add login Route

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -45,10 +45,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       expiresIn: "7d",
     });
 
-    res.json({ accessToken: token });
+    res.status(201).json({ accessToken: token });
   } catch (err) {
     return next(createHttpError(500, "Error while signing the JWT Token."));
   }
 };
 
-export { createUser };
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(createHttpError(400, "All fields are required"));
+  }
+  let user: User | null;
+  try {
+    user = await userModel.findOne({ email: email });
+
+    if (!user) {
+      return next(createHttpError(404, "User not found!"));
+    }
+  } catch (err) {
+    return next(
+      createHttpError(500, "Unable able to find that email in our DB")
+    );
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return next(createHttpError(400, "Username or password incorrect"));
+  }
+
+  // if matched create access token
+  const token = sign({ sub: user._id }, config.jwtSecret as string, {
+    expiresIn: "7d",
+  });
+
+  res.json({
+    accessToken: token,
+  });
+};
+
+export { createUser, loginUser };
